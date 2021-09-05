21d6b7129acd01ceaad5509526e9e462816242e4
ivannaht <ivhontaryk@gmail.com>
2021-09-05 23:00:17 +0300

    Added task 10

---
+++ index.rb
@@ -0,0 +1,46 @@
+# Task1
+A = 50
+B = 150
+C = A + B
+p C
+puts('*'*80)
+
+# Task 2
+D = 2 + 2 * 2
+E = (2+2)*2
+p D
+p E
+puts('*'*80)
+
+# Task 3
+F = 64**(0.5)
+p F
+puts('*'*80)
+
+# Task 4, 5
+G_1 = B / A
+G_2 = B * A
+G_3 = B - A
+G_4 = B + A
+puts(G_1, G_2, G_3, G_4)
+puts("B>A: #{B>A}")
+puts("A<=B: #{A<=B}")
+puts("A>=B: #{A>=B}")
+puts("A<B: #{A<B}")
+puts("A<=>B: #{A<=>B}")
+puts('*'*80)
+
+# Task 7, 8
+puts("1==1: #{1==1}")
+puts("1==1.0: #{1==1.0}")
+puts('*'*80)
+
+# Task 9
+str_1 = "This is test"
+str_2 = "This is test"
+puts("str_1==str_2: #{str_1==str_2}")
+puts('*'*80)
+
+# Task 10, 11
+puts("1.eql?(1.0): #{1.eql?(1.0)}")
+puts("1.equal?(1): #{1.equal?(1)}")
